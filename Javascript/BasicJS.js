0f2f8047a327b8a40b9803b6ae61aa5dc24d7e58
Ayubaabdi <ayubabdiibraahim@gmail.com>
2024-11-05 00:12:26 +0300

    added Variables

--- Javascript/BasicJS.js
+++ Javascript/BasicJS.js
@@ -1,3 +1,30 @@
 // First Code Js
 
 console.log("this is js first code");
+
+// Variables 
+
+// var 
+
+var firstName = "Ayub"
+var age = 40;
+var idCard = 7474;
+
+console.log(firstName, age , idCard);
+
+// let 
+
+let Color = "red";
+let Amount = 2;
+let price = 10;
+
+console.log(Color, Amount, price)
+
+
+// const
+
+const fastName = "Ali"
+const middleName = "ahmed"
+const lastName = "Amin"
+
+console.log(fastName, middleName, lastName)
