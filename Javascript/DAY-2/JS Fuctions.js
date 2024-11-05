f6eb84076a5df84fb664181d137ca4da63baa702
Ayubaabdi <ayubabdiibraahim@gmail.com>
2024-11-05 06:54:27 +0300

    added Function Parameters and Arguments

--- Javascript/DAY-2/JS Fuctions.js
+++ Javascript/DAY-2/JS Fuctions.js
@@ -1,6 +1,18 @@
 // functions
 function greet() {
-    console.log('hello world this is a function')
+    console.log('hello world this is a function');
 }
 
 greet()
+
+
+// Function Parameters and Arguments
+
+function message(name) {
+
+    console.log('hello ' + name)
+
+}
+
+message("ayub");
+message("abdi");
