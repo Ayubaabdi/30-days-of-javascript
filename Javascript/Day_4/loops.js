7b333fcd4c25295d474399851d17477ffa902298
Ayubaabdi <ayubabdiibraahim@gmail.com>
2024-11-06 17:19:42 +0300

    added Meaningful Example Using Arrays

--- Javascript/Day_4/loops.js
+++ Javascript/Day_4/loops.js
@@ -13,3 +13,16 @@ for (let i = 0; i < 5; i++) {
 for (let x= 1; x<51; x++) {
     console.log(x)
 }
+
+
+// Meaningful Example Using Arrays
+
+  let classStudents = ["Ayub","Amina","Hawa", "salah","mohamud","Mohamed"]
+
+//   console.log(classStudents.length)
+
+for(let i = 0; i < classStudents.length; i++) {
+
+    console.log(classStudents[i])
+
+}
