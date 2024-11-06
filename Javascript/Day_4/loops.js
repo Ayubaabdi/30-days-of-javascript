67e35abc60d79383318774537a46b83256773023
Ayubaabdi <ayubabdiibraahim@gmail.com>
2024-11-07 02:32:53 +0300

    added do while loop

--- Javascript/Day_4/loops.js
+++ Javascript/Day_4/loops.js
@@ -1,47 +1,67 @@
-// Loop 
+// Loop
 
 // For loop
 
 for (let i = 0; i < 5; i++) {
-    console.log(i)
+  console.log(i);
 }
 
+// exercise
 
-
-// exercise 
-
-for (let x= 1; x<51; x++) {
-    console.log(x)
+for (let x = 1; x < 51; x++) {
+  console.log(x);
 }
-
 
 // Meaningful Example Using Arrays
 
-  let classStudents = ["Ayub","Amina","Hawa", "salah","mohamud","Mohamed"]
+let classStudents = ["Ayub", "Amina", "Hawa", "salah", "mohamud", "Mohamed"];
 
 //   console.log(classStudents.length)
 
-for(let i = 0; i < classStudents.length; i++) {
-
-    console.log(classStudents[i])
-
+for (let i = 0; i < classStudents.length; i++) {
+  console.log(classStudents[i]);
 }
 
 // Exercise
 
-const Fruits = ["apple", "banana", "mouse"]
+const Fruits = ["apple", "banana", "mouse"];
 
-for(let f= 0; f< Fruits.length; f++) {
-        console.log(Fruits[f])
+for (let f = 0; f < Fruits.length; f++) {
+  console.log(Fruits[f]);
 }
-
 
 // while loop
 
-let i= 0;
+let i = 0;
 
-while(i < 5) {
-console.log("Itaration Num" + i)
+while (i < 5) {
+  console.log("Itaration Num" + i);
 
-i++
+  i++;
 }
+
+// Exercise
+
+let x = 5;
+
+while (x > 0) {
+  console.log("this is num" + x);
+  x--;
+}
+
+//  Do While Loop
+
+// let v = 0;
+
+// do {
+//   console.log("numbers is " + v);
+//   v++;
+// } while (v < 5);
+
+let password;
+
+do {
+  password = prompt("enter you passerd");
+} while (password !== "1234");
+
+console.log("welcome user");
