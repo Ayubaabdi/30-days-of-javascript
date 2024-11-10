1b3a294f82ab7028ef1a8e0583fdde71ae30b76a
Ayubaabdi <ayubabdiibraahim@gmail.com>
2024-11-10 16:39:08 +0300

    added new file js problems

--- js problems/Problem-1.js
+++ js problems/Problem-1.js
@@ -1,1 +1,9 @@
-console.log("hello js")
+
+let  name = "Hamza" ;
+let age = 20;
+let student = true;
+let active = true 
+
+console.log("Name: " +name);
+console.log("Age : " +age);
+console.log(student && active);
