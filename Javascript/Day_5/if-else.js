caf2b4f01b589f0ca53d3b7e68ecaf932d470651
Ayubaabdi <ayubabdiibraahim@gmail.com>
2024-11-10 16:12:22 +0300

    added all code

--- Javascript/Day_5/if-else.js
+++ Javascript/Day_5/if-else.js
@@ -1,1 +1,56 @@
-console.log("if-else")
+// Conditional Statements (if-else)
+
+let age = 17;
+
+if (age >= 18) {
+  console.log("you're Adult");
+}
+
+// if-else Statement
+
+let ages = 10;
+
+if (ages >= 20) {
+  console.log("you're an adult");
+} else {
+  console.log("you're a minor");
+}
+
+// Exercise
+
+let score = 10;
+
+if (score >= 50) {
+  console.log("You're passed");
+} else {
+  console.log("you're fail");
+}
+
+// else if Statement 
+
+const aga = 20;
+
+if (age < 18) {
+    console.log("you're are child")
+
+}else if (aga < 20) {
+    console.log("you're an adult")
+} else {
+    console.log("you're Man")
+}
+
+
+// Exercise
+
+
+let temperature = -14;
+
+if (temperature < -0) {
+        console.log("Very Cold")
+} else if (temperature < 15) {
+    console.log("Cold")
+}else if (temperature < 25) {
+        console.log("warm")
+} else {
+    console.log("Hot")
+}
