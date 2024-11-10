77e7b390d93691672b7b1b14e96638b0de9c5f1b
Ayubaabdi <ayubabdiibraahim@gmail.com>
2024-11-11 02:22:12 +0300

    added line of code

--- Javascript/Day_5/if-else.js
+++ Javascript/Day_5/if-else.js
@@ -26,31 +26,51 @@ if (score >= 50) {
   console.log("you're fail");
 }
 
-// else if Statement 
+// else if Statement
 
 const aga = 20;
 
 if (age < 18) {
-    console.log("you're are child")
-
-}else if (aga < 20) {
-    console.log("you're an adult")
+  console.log("you're are child");
+} else if (aga < 20) {
+  console.log("you're an adult");
 } else {
-    console.log("you're Man")
+  console.log("you're Man");
 }
 
-
 // Exercise
-
 
 let temperature = -14;
 
 if (temperature < -0) {
-        console.log("Very Cold")
+  console.log("Very Cold");
 } else if (temperature < 15) {
-    console.log("Cold")
-}else if (temperature < 25) {
-        console.log("warm")
+  console.log("Cold");
+} else if (temperature < 25) {
+  console.log("warm");
 } else {
-    console.log("Hot")
+  console.log("Hot");
 }
+
+// Ternary Operator
+
+const message = age >= 30 ? "You're Adult" : "Your an Minor";
+
+console.log(message);
+
+// and Operator &&
+
+const inbox = (age = 18 && "this is an adult");
+
+console.log(inbox);    
+
+
+// Exercise Ternary Operator
+
+
+
+let exam = 69;
+let result = exam >= 70? "you're passed" : "you're fail"
+
+console.log(result)
+
--- js problems/Problem-1.js
+++ js problems/Problem-1.js
@@ -1,9 +1,24 @@
-
-let  name = "Hamza" ;
+let name = "Hamza";
 let age = 20;
 let student = true;
-let active = true 
+let active = true;
 
-console.log("Name: " +name);
-console.log("Age : " +age);
+console.log("Name: " + name);
+console.log("Age : " + age);
 console.log(student && active);
+
+
+
+
+
+
+// Challenge two
+
+let a = 10;
+let b = 10;
+
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+
