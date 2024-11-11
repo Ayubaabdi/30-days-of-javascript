9ecfa2df80578889d6575b59bdc9b3dd9c8a709f
Ayubaabdi <ayubabdiibraahim@gmail.com>
2024-11-11 06:39:46 +0300

    added Object Destructuring

--- Javascript/Day_7/ES6.js
+++ Javascript/Day_7/ES6.js
@@ -1,30 +1,38 @@
 // Template Literals
 
+const name = "Ayub";
+const lastName = "Abdi";
+const city = "Mogadishu";
 
-const name = "Ayub"
-const lastName = "Abdi"
-const city = "Mogadishu"
+let message = `hi mr ${name} ${lastName} welcome to ${city}`;
 
+console.log(message);
 
-let message = `hi mr ${name} ${lastName} welcome to ${city}`
-
-console.log(message)
-
-
-// Array Destructuring  
+// Array Destructuring
 
 let fruits = ["apple", "banana", "jerry"];
 
-const  [ayub, banana, jerry] = fruits
+const [ayub, banana, jerry] = fruits;
 
-console.log(ayub)
+console.log(ayub);
 
 // exercise Array Destructuring
 
 let numbers = ["Red", "blue", "green"];
 
-const [red, blue, green] = numbers
+const [red, blue, green] = numbers;
 
-const messages = `this color is ${red} and this color is ${blue}`
+const messages = `this color is ${red} and this color is ${blue}`;
 
-console.log(messages)
+console.log(messages);
+
+
+// Object Destructuring
+
+const  list = { names: "Ayub", age: 20, citye: "Mogadishu" };
+
+const {names,age,citye } = list
+
+console.log(name);
+console.log(age);
+console.log(city);
