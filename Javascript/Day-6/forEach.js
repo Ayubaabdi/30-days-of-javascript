ae30c25e21e6bd1fafc15c3e6917d348cd24aabf
Ayubaabdi <ayubabdiibraahim@gmail.com>
2024-11-11 03:05:24 +0300

    added forEach() Method

--- Javascript/Day-6/forEach.js
+++ Javascript/Day-6/forEach.js
@@ -1,1 +1,24 @@
-console.log("hello yow!!");
+// forEach() Method
+
+const fruits = ["apple", "banana", "jerry"];
+
+fruits.forEach((fruits) => {
+  console.log("this is " + fruits);
+});
+
+// Exercise forEach() Method
+
+const numbers = [1, 2, 3, 4, 5];
+
+numbers.forEach((numbers) => {
+  console.log("this is Number " + numbers);
+});
+
+
+// Map
+
+let number = [1,2,3,4,5]
+
+const newnumbers = number.map((number)=> number *2)
+
+console.log(newnumbers)
