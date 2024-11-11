15026c4f7638be4ab39090ef47ffda70dece4c01
Ayubaabdi <ayubabdiibraahim@gmail.com>
2024-11-11 06:26:35 +0300

    added Array Destructuring and exercise

---
+++ Javascript/Day_7/ES6.js
@@ -0,0 +1,30 @@
+// Template Literals
+
+
+const name = "Ayub"
+const lastName = "Abdi"
+const city = "Mogadishu"
+
+
+let message = `hi mr ${name} ${lastName} welcome to ${city}`
+
+console.log(message)
+
+
+// Array Destructuring  
+
+let fruits = ["apple", "banana", "jerry"];
+
+const  [ayub, banana, jerry] = fruits
+
+console.log(ayub)
+
+// exercise Array Destructuring
+
+let numbers = ["Red", "blue", "green"];
+
+const [red, blue, green] = numbers
+
+const messages = `this color is ${red} and this color is ${blue}`
+
+console.log(messages)
