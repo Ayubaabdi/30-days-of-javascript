e119fc5225a8ffeb1d5b628cd146c065ac6f6257
Ayubaabdi <ayubabdiibraahim@gmail.com>
2024-11-11 07:02:08 +0300

    added Default Parameters and exercise

--- Javascript/Day_7/ES6.js
+++ Javascript/Day_7/ES6.js
@@ -26,13 +26,38 @@ const messages = `this color is ${red} and this color is ${blue}`;
 
 console.log(messages);
 
-
 // Object Destructuring
 
-const  list = { names: "Ayub", age: 20, citye: "Mogadishu" };
+const list = { names: "Ayub", age: 20, citye: "Mogadishu" };
 
-const {names,age,citye } = list
+const { names, age, citye } = list;
 
 console.log(name);
 console.log(age);
 console.log(city);
+
+// exercise Object Destructuring
+
+const car = { make: "Toyota", Model: "Corrala", Year: 2024 };
+
+const { make, Model } = car;
+
+console.log(make);
+console.log(Model);
+
+// Default Parameters
+
+function person  (name ="Guest") {
+  console.log(`hello ${name}`)
+}
+
+person("Amina")
+
+
+// exercise  Default Parameters
+
+function calculateArea (width, height=5) {
+        console.log(`width is ${width} and height is ${height}`)
+}
+
+calculateArea(5)
