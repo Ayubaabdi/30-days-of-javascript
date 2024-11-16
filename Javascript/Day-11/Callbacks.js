89e8327b112c08dd27d80feff01a03999fb860cf
Ayubaabdi <ayubabdiibraahim@gmail.com>
2024-11-16 06:24:57 +0300

    added callback example

--- Javascript/Day-11/Callbacks.js
+++ Javascript/Day-11/Callbacks.js
@@ -10,3 +10,21 @@ function userDataInfo(callback) {
 
 userDataInfo(great);
 
+// Another Callback example
+
+function operate (a, b, callback) {
+  return callback(a, b)
+  
+}
+
+function add (a, b) {
+  return a+b;
+}
+
+function substaction (a,b) {
+  return a - b
+}
+
+
+console.log("additional" , operate(10, 20, add));
+console.log("Substraction" , operate(20, 10, substaction));
