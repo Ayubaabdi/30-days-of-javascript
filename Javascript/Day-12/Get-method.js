5887074fd5f85ca07585c211c182d18295351792
Ayubaabdi <ayubabdiibraahim@gmail.com>
2024-11-16 08:08:51 +0300

    Added new https get method

--- Javascript/Day-12/Get-method.js
+++ Javascript/Day-12/Get-method.js
@@ -1,1 +1,20 @@
-console.log("HTTP");
+async function fetchData () {
+    try {
+
+        console.log("started fetching data")
+
+        const response = await fetch('https://jsonplaceholder.typicode.com/posts')
+        
+        if(!response.ok) {
+            throw new Error (`HTTP Error! status ${response.status} `);
+        }
+
+        const data = await response.json()
+        console.log(data)
+
+    }catch (error) {
+        console.log(error)
+    }
+}
+
+fetchData()
