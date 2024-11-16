bf29607710d91073cbf7dc270a383e9e352913b4
Ayubaabdi <ayubabdiibraahim@gmail.com>
2024-11-16 09:40:44 +0300

    added post method

--- Javascript/Day-12/Get-method.js
+++ Javascript/Day-12/Get-method.js
@@ -1,20 +1,45 @@
-async function fetchData () {
-    try {
+async function fetchData() {
+  try {
+    console.log("started fetching data");
 
-        console.log("started fetching data")
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts");
 
-        const response = await fetch('https://jsonplaceholder.typicode.com/posts')
-        
-        if(!response.ok) {
-            throw new Error (`HTTP Error! status ${response.status} `);
-        }
-
-        const data = await response.json()
-        console.log(data)
-
-    }catch (error) {
-        console.log(error)
+    if (!response.ok) {
+      throw new Error(`HTTP Error! status ${response.status} `);
     }
+
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-fetchData()
+fetchData();
+
+async function postData() {
+  try {
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({
+        title: "whats is the title of post",
+        body: "hi am the post",
+        user: 1,
+      }),
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP Error! ${response.status}`);
+    }
+    const data = await response.json();
+
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+postData();
