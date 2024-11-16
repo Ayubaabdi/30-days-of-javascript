de88f3e497b9e6d1ca60bdd3c57f09e0b485320a
Ayubaabdi <ayubabdiibraahim@gmail.com>
2024-11-17 02:09:33 +0300

    added new querySelectors

---
+++ Javascript/Day-13/Dom.js
@@ -0,0 +1,28 @@
+// const header = document.getElementById("header");
+
+// console.log(header);
+
+// const text = document.getElementsByClassName("text");
+
+// console.log(text);
+
+// const button = document.getElementsByTagName('button')[1]
+
+// console.log(button)
+
+// using by querySelectors
+
+const header = document.querySelector('#header');
+
+console.log(header)
+
+// const text = document.querySelector('.text');
+
+// console.log(text)
+
+const text = document.querySelectorAll('.text')
+
+console.log(text)
+
+const button = document.querySelectorAll('button')
+console.log(button)
