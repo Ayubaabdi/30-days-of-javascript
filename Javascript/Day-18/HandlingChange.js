692426f2580e1b3bee9e9e67aec6ea9c328b9dc9
Ayubaabdi <ayubabdiibraahim@gmail.com>
2024-11-19 17:54:48 +0300

    added new user change

--- Javascript/Day-18/HandlingChange.js
+++ Javascript/Day-18/HandlingChange.js
@@ -1,8 +1,16 @@
-const Color = document.querySelector('#Color');
-const selectedColor = document.querySelector("#selectedColor")
+const Color = document.querySelector("#Color");
+const selectedColor = document.querySelector("#selectedColor");
+const userName = document.querySelector("#userName");
+const currentUser = document.querySelector("#currentUser")
 
-Color.addEventListener('change', function() {
-    console.log("Selected Color" , Color.value);
+Color.addEventListener("change", function () {
+  console.log("Selected Color", Color.value);
 
-    selectedColor.textContent = `selected Color : ${Color.value}`
-})
+  selectedColor.textContent = `selected Color : ${Color.value}`;
+
+  userName.addEventListener('change', function() {
+        console.log("Current Name is " , userName.value)
+
+        currentUser.textContent = `Current Name is : ${userName.value}`
+  })
+});
