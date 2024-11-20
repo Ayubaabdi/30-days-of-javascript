e0a8337c12a76fb025a1d5eb5248dbab1953347f
Ayubaabdi <ayubabdiibraahim@gmail.com>
2024-11-21 00:16:14 +0300

    added all form validation compeletly

--- Javascript/Day-20/Validation.js
+++ Javascript/Day-20/Validation.js
@@ -1,31 +1,46 @@
-const form = document.querySelector("#FormValiation")
+const form = document.querySelector("#FormValidation");
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const username = document.querySelector("#username").value;
+  const email = document.querySelector("#email").value;
+  const password = document.querySelector("#password").value;
+  const confirmpassword = document.querySelector("#confirmpassword").value;
+
+  const error = document.querySelector("#error");
+
+  // UserName section
+  error.textContent = `hello Mr ${username}`;
+
+  if (username === "") {
+    error.textContent = "UserName Required";
+    return;
+  }
+  // Email section
+
+  const emailpattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+  if (!email.match(emailpattern)) {
+    (error.textContent = "Your Email valid Incorrect please check");
+
+    return;
+  }
+
+   error.textContent = "Your data Successfully"
+
+//    password section
+
+   if(password.length < 8) {
+    error.textContent = "Your password at least 8 digits"
+    return;
+   }
+
+//    confirm password
+
+   if(password !== confirmpassword) {
+    error.textContent = "Your password Not Matched"
+   }
 
 
-form.addEventListener('submit' , function(event) {
-    event.preventDefault()
-
-    const username = document.querySelector("#username").value;
-    const email =  document.querySelector("#email").value;
-    const password = document.querySelector("#password").value;
-    const confirmpassword = document.querySelector("#confirmpassword").value;
-
-    const error = document.querySelector("#error")
-
-   
-
-    // UserName section
-    error.textContent = `hello Mr ${username}`
-   
-    
-    if(username === '') {
-        error.textContent = "UserName Required";
-        return
-    }
-      // Email section
-
-      if(email === '') {
-        error.textContent = "Email Required"
-        return
-      }
-
-})
+});
