4e97faf3820d2f0259be8b3d1e2d7dbbb58fb79c
Ayubaabdi <ayubabdiibraahim@gmail.com>
2024-11-20 23:37:01 +0300

    added sucess first step validation user

--- Javascript/Day-20/Validation.js
+++ Javascript/Day-20/Validation.js
@@ -1,1 +1,31 @@
 const form = document.querySelector("#FormValiation")
+
+
+form.addEventListener('submit' , function(event) {
+    event.preventDefault()
+
+    const username = document.querySelector("#username").value;
+    const email =  document.querySelector("#email").value;
+    const password = document.querySelector("#password").value;
+    const confirmpassword = document.querySelector("#confirmpassword").value;
+
+    const error = document.querySelector("#error")
+
+   
+
+    // UserName section
+    error.textContent = `hello Mr ${username}`
+   
+    
+    if(username === '') {
+        error.textContent = "UserName Required";
+        return
+    }
+      // Email section
+
+      if(email === '') {
+        error.textContent = "Email Required"
+        return
+      }
+
+})
